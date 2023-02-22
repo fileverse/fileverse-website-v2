27472e365a18eff34c441448c52db933741dc04d
Shankar Tiwari <tiwarishankar587@gmail.com>
2023-02-22 14:21:01 +0530

    refactor code

--- components/RiveAnimation.tsx
+++ components/RiveAnimation.tsx
@@ -2,19 +2,11 @@ import React from 'react';
 import Rive, { useRive, useStateMachineInput } from 'rive-react';
 
 export default function RiveLaunchPortal() {
-  const STATE_MACHINE_NAME = 'Hover animations';
-  const INPUT_NAME = 'Hover';
   const { rive, RiveComponent } = useRive({
     src: 'Fileverse.riv',
     autoplay: true,
     animations: 'Hover',
   });
-
-  const onClickInput = useStateMachineInput(
-    rive,
-    STATE_MACHINE_NAME,
-    INPUT_NAME
-  );
 
   return (
     <div className="h-[230px] w-[200px]">
