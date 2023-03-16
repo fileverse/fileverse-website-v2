2dca830eecc3bee507bf64c37f571bd562b1f9e0
Maitra Khatri <54719132+maitrakhatri@users.noreply.github.com>
2023-03-16 22:42:06 +0530

    Copyright footer (#17)
    * Copy changes on Portal Landing Page
    
    * updated FAQs
    
    * added new images
    
    * new hero image and fixed typo
    
    * HQ portal hero image
    
    * feat: copyright footer

---
+++ components/CopyrightFooter.tsx
@@ -0,0 +1,40 @@
+export const CopyrightFooter = () => {
+  return (
+    <div className="bg-[ffffff] text-[#777777] flex px-4 py-2 justify-center items-center gap-4 font-semibold">
+      <p> &#169; 2022-2023 Fileverse ltd.</p>
+      <p>|</p>
+      <p>
+        <a
+          href="https://portal.fileverse.io/#/acceptable-use-policy"
+          target="_blank"
+          className="text-[#303030] font-bold"
+          rel="noreferrer"
+        >
+          Acceptable Use
+        </a>
+      </p>
+      <p>|</p>
+      <p>
+        <a
+          href="https://portal.fileverse.io/#/terms-and-condition"
+          target="_blank"
+          className="text-[#303030] font-bold"
+          rel="noreferrer"
+        >
+          Terms
+        </a>
+      </p>
+      <p>|</p>
+      <p>
+        <a
+          href="https://portal.fileverse.io/#/privacy-policy"
+          target="_blank"
+          className="text-[#303030] font-bold"
+          rel="noreferrer"
+        >
+          Privacy
+        </a>
+      </p>
+    </div>
+  );
+};
