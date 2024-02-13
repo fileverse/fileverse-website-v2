6c2d1ba9e3610106941a16267a130b16f3b846f7
Nadeem <nadeem@fileverse.io>
2024-02-13 22:10:21 +0530

    update chains

--- utils/wagmi-config.ts
+++ utils/wagmi-config.ts
@@ -1,13 +1,27 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { http } from 'wagmi';
-import { mainnet } from 'wagmi/chains';
+import {
+  gnosis,
+  goerli,
+  mainnet,
+  optimism,
+  polygon,
+  polygonMumbai,
+  sepolia,
+} from 'wagmi/chains';
 
 export const rainbowkitConfig = getDefaultConfig({
   appName: 'fileverse.io',
   projectId: 'ff71575e67eb530fe9b1a99d0a1d8a37',
-  chains: [mainnet],
+  chains: [mainnet, sepolia, gnosis, polygon, polygonMumbai, optimism, goerli],
   transports: {
     [mainnet.id]: http(),
+    [sepolia.id]: http(),
+    [gnosis.id]: http(),
+    [polygon.id]: http(),
+    [polygonMumbai.id]: http(),
+    [optimism.id]: http(),
+    [goerli.id]: http(),
   },
   ssr: true,
 });
