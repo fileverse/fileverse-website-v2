8bef580fd603907e67db3534e8476076da6b6016
Nadeem <nadeem@fileverse.io>
2024-03-29 13:50:09 +0530

    fix anchor tags

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 module.exports = {
   reactStrictMode: false,
-  output: 'standalone',
+  // output: 'standalone',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
